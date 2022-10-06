233dd472bf3286536a7ef14e229ec4f00b50f89e
WilliamARivas <willarivas@yahoo.com>
2022-10-06 00:58:39 +0000

    starting project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	const [list, setList] = useState([]);
+
+	useEffect(() => {
+		getList()
+	},[])
+
+	const getList = () =>{
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/williamarivas")
+		.then(response => response.json())
+		.then(result => setList(result))
+		.catch(error => console.log("error: ", error))
+	}
+
+
+	const addTask = (myTask) =>{
+		const newList = [...list, myTask]
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/williamarivas", {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'Application/json'
+			},
+			body: JSON.stringify(newList),
+			redirect: 'follow'
+		})
+			.then(response => response.json())
+			.then(result => getList())
+			.catch(error => console.log("error: ", error))
+	}
+
+	console.log(list)
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			{list.map((task, i) =>{
+				return <p key={i}>{task.label}<span>     x</span></p>
+			})}
+			<button
+				onClick={()=>addTask({label: "eat", done: false})}
+				className="btn btn-primary"
+			>
+				Add
+			</button>
 		</div>
 	);
 };
